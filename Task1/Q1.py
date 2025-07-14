42afb148a5cbfdc5e5293ca00021ea66d4bb6258
RohanSirr <shubhamgupta642004@gmail.com>
2025-07-14 11:53:55 +0530

    Python codes

---
+++ Task1/Q1.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+addition = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtract}")
+print(f"Multiplication: {multiply}")
+print(f"Division: {divide}")
